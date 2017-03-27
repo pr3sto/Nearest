c9bd7f588585d1375f1ab1d8d56624252dcc83f6
pr3sto <pr3sto1377@gmail.com>
2017-03-28 01:19:04 +0300

    add google maps

--- Nearest/Nearest/MapFragment.cs
+++ Nearest/Nearest/MapFragment.cs
@@ -1,11 +1,29 @@
+using System;
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Gms.Maps;
 
 namespace Nearest
 {
+    public class MyOnMapReady : Java.Lang.Object, IOnMapReadyCallback
+    {
+        public GoogleMap Map { get; private set; }
+
+        public event EventHandler MapReady;
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            Map = googleMap;
+            MapReady?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
     public class MapFragment : Fragment
     {
+        private MapView mapView;
+        private GoogleMap map;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,7 +43,48 @@ namespace Nearest
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            return inflater.Inflate(Resource.Layout.map, container, false);
+            View view = inflater.Inflate(Resource.Layout.map, container, false);
+
+            mapView = (MapView)view.FindViewById(Resource.Id.map_view);
+            mapView.OnCreate(savedInstanceState);
+            mapView.OnResume(); // needed to get the map to display immediately
+            MapsInitializer.Initialize(Activity.ApplicationContext);
+
+            var mapReadyCallback = new MyOnMapReady();
+            mapReadyCallback.MapReady += (sender, args) =>
+            {
+                map = mapReadyCallback.Map;
+                map.MapType = GoogleMap.MapTypeNormal;
+                map.MyLocationEnabled = true;
+            };
+
+            mapView.GetMapAsync(mapReadyCallback);
+
+            return view;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            mapView.OnResume();
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            mapView.OnPause();
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            mapView.OnDestroy();
+        }
+
+        public override void OnLowMemory()
+        {
+            base.OnLowMemory();
+            mapView.OnLowMemory();
         }
     }
 }
